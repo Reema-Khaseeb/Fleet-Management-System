12a2366dc77d304b703fcb23815e8788b0e2280f
Reema Khaseeb <reemafkhaseeb@gmail.com>
2024-05-13 04:22:52 +0300

    Create `GVAR`

---
+++ FleetManagementSystem.Services/GVAR.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Concurrent;
+using System.Data;
+
+namespace FleetManagementSystem.Services;
+
+public class GVAR
+{
+    public ConcurrentDictionary<string, ConcurrentDictionary<string, string>> DicOfDic { get; set; }
+    public ConcurrentDictionary<string, DataTable> DicOfDT { get; set; }
+
+    public GVAR()
+    {
+        DicOfDic = new ConcurrentDictionary<string, ConcurrentDictionary<string, string>>();
+        DicOfDT = new ConcurrentDictionary<string, DataTable>();
+    }
+}
