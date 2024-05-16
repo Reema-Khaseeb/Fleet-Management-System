e92d95553a02f236b7030e604988dc9c47396d00
Reema Khaseeb <reemafkhaseeb@gmail.com>
2024-05-16 04:40:09 +0300

    `Driver` CRUD operations

--- FleetManagementSystem.Api/Program.cs
+++ FleetManagementSystem.Api/Program.cs
@@ -32,6 +32,7 @@ public class Program
         // Add services to the container.
         builder.Services.AddSingleton<IDatabaseConnection, DatabaseConnection>();
         builder.Services.AddScoped<VehicleService>();
+        builder.Services.AddScoped<DriverService>();
 
         // Add controllers to the services container.
         builder.Services.AddControllers().AddJsonOptions(options =>
