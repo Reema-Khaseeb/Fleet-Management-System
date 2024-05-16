95e65e798eb1c62e8faf993a5f7c12f8f034833d
Reema Khaseeb <reemafkhaseeb@gmail.com>
2024-05-16 04:43:12 +0300

    `VehiclesInformations` CRUS Operations

--- FleetManagementSystem.Api/Program.cs
+++ FleetManagementSystem.Api/Program.cs
@@ -32,6 +32,7 @@ public class Program
         // Add services to the container.
         builder.Services.AddSingleton<IDatabaseConnection, DatabaseConnection>();
         builder.Services.AddScoped<VehicleService>();
+        builder.Services.AddScoped<VehicleInformationService>();
         builder.Services.AddScoped<DriverService>();
 
         // Add controllers to the services container.
