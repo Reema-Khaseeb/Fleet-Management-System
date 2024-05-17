5b20837d9bdd932038146b2c2254d92806ed3a9b
Reema Khaseeb <reemafkhaseeb@gmail.com>
2024-05-17 18:16:24 +0300

    `GET` with more details for `Vehicle`, `Vehicles`, `VehicleRouteHistory`

--- FleetManagementSystem.Api/Controllers/VehiclesController.cs
+++ FleetManagementSystem.Api/Controllers/VehiclesController.cs
@@ -22,13 +22,6 @@ public class VehiclesController : ControllerBase
         return gvarResponse.DicOfDic["Tags"]["STS"] == "1" ? Ok(gvarResponse) : BadRequest(gvarResponse);
     }
 
-    [HttpGet]
-    public IActionResult GetVehiclesGvar()
-    {
-        var gvarResponse = _vehicleService.GetVehicles();
-        return gvarResponse.DicOfDic["Tags"]["STS"] == "1" ? Ok(gvarResponse) : BadRequest(gvarResponse);
-    }
-
     [HttpDelete]
     public IActionResult DeleteVehicle([FromBody] GVAR gvar)
     {
@@ -40,6 +33,27 @@ public class VehiclesController : ControllerBase
     public IActionResult UpdateVehicle([FromBody] GVAR gvar)
     {
         var gvarResponse = _vehicleService.UpdateVehicle(gvar);
+        return gvarResponse.DicOfDic["Tags"]["STS"] == "1" ? Ok(gvarResponse) : BadRequest(gvarResponse);
+    }
+
+    [HttpGet("{vehicleID}")]
+    public IActionResult GetVehicleDetails(long vehicleID)
+    {
+        var gvarResponse = _vehicleService.GetVehicleDetails(vehicleID);
+        return gvarResponse.DicOfDic["Tags"]["STS"] == "1" ? Ok(gvarResponse) : BadRequest(gvarResponse);
+    }
+
+    [HttpGet]
+    public IActionResult GetAllVehicles()
+    {
+        var gvarResponse = _vehicleService.GetVehicles();
+        return gvarResponse.DicOfDic["Tags"]["STS"] == "1" ? Ok(gvarResponse) : BadRequest(gvarResponse);
+    }
+
+    [HttpGet("{vehicleID}/routehistory")]
+    public IActionResult GetVehicleRouteHistory(long vehicleID, [FromQuery] long startEpoch, [FromQuery] long endEpoch)
+    {
+        var gvarResponse = _vehicleService.GetVehicleRouteHistory(vehicleID, startEpoch, endEpoch);
         return gvarResponse.DicOfDic["Tags"]["STS"] == "1" ? Ok(gvarResponse) : BadRequest(gvarResponse);
     }
 }
